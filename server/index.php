5cf964f96e7843a7629c9bce1d838a0f51a55e5a
teddy gauthier <teddy.gauthier@outlook.com>
2021-04-16 14:31:29 +0200

    feat : comunication client serveur

--- server/index.php
+++ server/index.php
@@ -2,5 +2,7 @@
     header('Content-Type: text/plain; charset=utf-8');
     $data = json_decode(file_get_contents('php://input'));
     
-    echo $data->data[0];
+    foreach ($data->data as $k => $v) {
+        echo preg_match()
+    }
 ?>
