59e89588ec699cbb2eb44bcd9b914b9f48b62eb2
LordPax <capitaine_ted@hotmail.fr>
2021-04-19 17:49:39 +0200

    maj : index.php

--- link.sh
+++ link.sh
@@ -1,1 +1,1 @@
-ln -s /home/student/Documents/verif-marionnet/server/index.php /var/www/html/index.php
+ln -s /home/lordpax/Documents/Programmation/Bash/verif-marionnet/server/index.php /srv/http/verifServer/index.php
--- server/index.php
+++ server/index.php
@@ -1,7 +1,12 @@
 <?php
     header('Content-Type: text/plain; charset=utf-8');
-    // if (strlen($data) == 0) die(1) else echo 'Ok';
-    $data = json_decode(file_get_contents('php://input'));
+
+    $input = file_get_contents('php://input');
+    if (strlen($input) == 0) die(1);
+
+    $data = json_decode($input);
+    $save = "";
+
     $file = fopen('exempleTP1_requetes.txt', 'r');
 
     $content = [];
@@ -10,9 +15,25 @@
         if (preg_match('/^[^#]/', $line) == 1 && strlen($line) > 1)
             array_push($content, explode(';', $line));
     }
-    # echo var_dump($content);
+    fclose($file);
+
+    $totPts = 0;
+    $note = 0;
     
     foreach ($data->data as $k => $v) {
-        echo $content[$k][0].' : '.(preg_match('/'.trim($content[$k][3]).'/', $v) == 1 ? 'Ok' : 'Not ok')."\n";
+        $bareme = isset($content[$k][4]) ? (int)trim($content[$k][4]) : 1;
+        $pts = preg_match('/'.trim($content[$k][3]).'/', $v) === 1 ? $bareme : 0;
+
+        $totPts += $bareme;
+        $note += $pts;
+
+        $save .= $content[$k][0].' : '.$pts.'/'.$bareme."\n";
     }
+
+    $note20 = round(20 * $note / $totPts, 2);
+    $save .= "Your grade is $note/$totPts => $note20/20 \n";
+
+    // file_put_contents('log/marionnet.log', $save, FILE_APPEND);
+
+    echo $save;
 ?>
