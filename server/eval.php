fcba8557da11a7163bf075f904b2bf5d922a00fd
LordPax <capitaine_ted@hotmail.fr>
2021-04-30 17:44:01 +0200

    maj : server

--- server/eval.php
+++ server/eval.php
@@ -4,6 +4,53 @@
     $input = file_get_contents('php://input');
     if (strlen($input) == 0) die(1);
 
+    /**
+     * fonction qui choisie la couleur en fonction du nombre de points
+     * 
+     * @param pts nombre de points
+     * @param bareme la note max
+     * @param range marge d'erreur
+     * @param string une couleur
+     */
+    function chooseColor($pts, $bareme, $range = 0) : string {
+        $col = "";
+
+        if ($pts >= $bareme - $range) $col = "\e[32m"; // vert si tout des points
+        else if ($pts <= 0 + $range) $col = "\e[31;1m"; // rouge si aucun des points
+        else $col = "\e[93;1m"; // jaune entre les 2
+
+        return $col;
+    }
+
+    /**
+     * fonction qui vérifie la validité d'une réponse à condition qu'un de ces mot-clé exist (regex, equal, default)
+     * 
+     * @param resp objet contenant un mot-clé (regex, equal, default) avec la réponse attendu
+     * @param value reponse obtenu
+     * @return int 1 ou 0 en fonction de si la reponse est valide ou non
+     */
+    function validResponse(object $resp, string $value) : int { 
+        $valid = 0;
+
+        if (isset($resp->regex)) // regex keyword
+            $valid = preg_match('/'.$resp->regex.'/', $value);
+        else if (isset($resp->equal)) // equal keyword
+            $valid = $resp->equal === $value ? 1 : 0;
+        else // default keyword
+            $valid = 1;
+
+        return $valid;
+    }
+
+    // function writeLog(string $content, string $file, int $mode) {
+    //     if ($mode == 1)
+    //         $log = " * IP:$ip; Date:$date; Note:$note/$totPts; Note20:$note20/20; firstName:$data->firstName; name:$data->name; idExam:$data->idExam\n";
+    //     else
+    //         $log = " * IP:$ip; Date:$date; Note:$note/$totPts; Note20:$note20/20\n";
+
+    //     file_put_contents($logFile, $log, FILE_APPEND);
+    // }
+
     $data = json_decode($input);
     $source = $data->source; 
     $mode = $data->examMode;
@@ -11,8 +58,8 @@
     $normalLogFile='/home/gauthier/verif-marionnet/server/log/normal.log';
     $examLogFile='/home/gauthier/verif-marionnet/server/log/exam.log';
     $logFile = $mode === 1 ? $examLogFile : $normalLogFile;
-    $requestFile='/home/gauthier/public_html/'.$source.'/exempleTP1_requetes.json';
-    // $requestFile='/srv/http/server/exempleTP1_requetes.json';
+    // $requestFile='/home/gauthier/public_html/'.$source.'/exempleTP1_requetes.json';
+    $requestFile='/srv/http/server/exempleTP1_requetes.json';
 
     $show = '';
     $log = '';
@@ -27,17 +74,14 @@
     $valid = 0;
     $pts = 0;
     $comment = "";
+    $color="";
     
     foreach ($data->data as $k => $v) { 
         $bareme = !empty($content[$k]->bareme) ? $content[$k]->bareme : 1;
 
         for ($i = 0; $valid === 0 && $i < count($content[$k]->responses); $i++) {
-            if (isset($content[$k]->responses[$i]->regex))
-                $valid = preg_match('/'.$content[$k]->responses[$i]->regex.'/', $v);
-            else if (isset($content[$k]->responses[$i]->equal))
-                $valid = $content[$k]->responses[$i]->equal === $v ? 1 : 0;
-            else
-                $valid = 1;
+            $valid = validResponse($content[$k]->responses[$i], $v);
+            $color = chooseColor($content[$k]->responses[$i]->pts, $bareme);
             
             if ($valid === 1) {
                 $pts = $content[$k]->responses[$i]->pts;
@@ -48,23 +92,24 @@
         $totPts += $bareme;
         $note += $pts;
  
-        $show .= $content[$k]->label." \t ".$pts.'/'.$bareme." \t".$comment."\n";
+        $show .= $content[$k]->label." \t $color$pts/$bareme \t $comment\e[0m\n";
 
         $pts = 0;
         $comment = "";
         $valid = 0;
-
     }
 
     $note20 = round(20 * $note / $totPts, 2);
-    $show .= "Your grade is $note/$totPts => $note20/20 \n";
+    $color = chooseColor($note20, 20, 5);
 
-    if ($mode == 1)
-        $log = " * IP:$ip; Date:$date; Note:$note/$totPts; Note20:$note20/20; firstName:$data->firstName; name:$data->name; idExam:$data->idExam\n";
-    else
-        $log = " * IP:$ip; Date:$date; Note:$note/$totPts; Note20:$note20/20\n";
+    $show .= "\e[1mYour grade is $note/$totPts => $color$note20/20\e[0m\n";
 
-    file_put_contents($logFile, $log, FILE_APPEND);
+    // if ($mode == 1)
+    //     $log = " * IP:$ip; Date:$date; Note:$note/$totPts; Note20:$note20/20; firstName:$data->firstName; name:$data->name; idExam:$data->idExam\n";
+    // else
+    //     $log = " * IP:$ip; Date:$date; Note:$note/$totPts; Note20:$note20/20\n";
+
+    // file_put_contents($logFile, $log, FILE_APPEND);
 
     echo $show;
 ?>
