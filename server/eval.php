b161636f0bb9b54e70d5ae68e4f89904cca34e21
teddy gauthier <teddy.gauthier@outlook.com>
2021-05-12 17:43:46 +0200

    maj : server

--- server/eval.php
+++ server/eval.php
@@ -23,6 +23,18 @@
         return $col;
     }
 
+
+    function chooseColorWithType(string $ctype, $code = 0) : string {
+        $col = "";
+
+        if ($ctype === "good") $col = $code === 0 ? "\e[32m" : "green";
+        else if ($ctype === "wrong") $col = $code === 0 ? "\e[31;1m" : "red";
+        else if ($ctype === "partial") $col = $code == 0 ? "\e[93;1m" : "orange";
+	else $col = $code == 0 ? "\e[34m" : "blue"; 
+
+        return $col;
+    }
+
     /**
      * fonction qui vérifie la validité d'une réponse à condition qu'un de ces mot-clé exist (regex, equal, default)
      * 
@@ -57,7 +69,7 @@
     // echo $input;
     // var_dump($data);
 
-    $idExam= $data->idExam; 
+    $idExam= $data->idExam;
     $mode = $data->examMode;
     $graph = $data->graph;
     
@@ -71,7 +83,7 @@
     // $requestFile='/srv/http/server/exempleTP1_requetes.json';
 
     if (!file_exists("$logDir/$idExam")) {
-        mkdir("$logDir/$idExam/"); 
+        mkdir("$logDir/$idExam/");
         mkdir("$logDir/$idExam/normal/");
         mkdir("$logDir/$idExam/exam/");
     }
@@ -92,6 +104,7 @@
     $questionLog = "";
     $tolerance = 1; // nombre d'erreur accepté
     $nbErr = 0;
+    $type = "";
     
     foreach ($data->data as $k => $v) { 
         $bareme = $content[$k]->bareme !== 0 ? $content[$k]->bareme : $content[$k]->responses[0]->pts;
@@ -99,24 +112,31 @@
         if ($nbErr <= $tolerance) {
             for ($i = 0; $valid === 0 && $i < count($content[$k]->responses); $i++) {
                 $valid = validResponse($content[$k]->responses[$i], $v);
-                $color = chooseColor($content[$k]->responses[$i]->pts, $bareme, 0, $graph);
-            
+                #$color = chooseColor($content[$k]->responses[$i]->pts, $bareme, 0, $graph); 
+                $color = chooseColorWithType($content[$k]->responses[$i]->type, $graph);
                 if ($valid === 1) {
                     $pts = $content[$k]->responses[$i]->pts;
                     $comment = $content[$k]->responses[$i]->comment;
+                    $type = $content[$k]->responses[$i]->type;
                 }
             }
         }
         else {
             $pts = 0;
             $comment = "Niveau de tolérance aux erreurs dépassées (tolérance : $tolerance)";
+            $type = $content[$i]->responses[0]->type;
+            if (type === "info") 
+                $color = $graph === 0 ? "\e[34m" : "blue";
+            else
+                $color = $graph === 0 ? "\e[31;1m" : "red";
         }
 
+        if ($type !== "info") { // les pts ne sont pas compté
+            $totPts += $bareme;
+            $note += $pts;
+        }
 
-        $totPts += $bareme;
-        $note += $pts;
-
-	if ($pts === 0) $nbErr++;
+        if ($pts === 0) $nbErr++;
 
         $questionLog .= "; ".$content[$k]->label.":$pts/$bareme";
  
@@ -128,6 +148,7 @@
         $pts = 0;
         $comment = "";
         $valid = 0;
+        $type = "";
     }
 
     $note20 = round(20 * $note / $totPts, 2);
